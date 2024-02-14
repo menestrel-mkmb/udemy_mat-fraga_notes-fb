a7f58d8ede6a728b279a8f25a61ea2c16aa4f6fb
Michael Kevin <michaelkmb@hotmail.com>
2024-02-14 09:45:13 -0300

    feat using BrowserRouter on App;

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import RoutesApp from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   );
 }
 
