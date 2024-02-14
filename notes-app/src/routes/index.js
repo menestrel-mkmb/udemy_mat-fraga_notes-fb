da864e70b73b48fbb480ae185f3f90bf86e2bddc
Michael Kevin <michaelkmb@hotmail.com>
2024-02-14 17:39:16 -0300

    misc adding Notes Page to Router;

--- notes-app/src/routes/index.js
+++ notes-app/src/routes/index.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Notes from "../pages/Notes";
 
 export default function RoutesApp(){
     return(
         <Routes>
             <Route path='/' element={ <Home /> } />
+            <Route path='/notes' element={ <Notes /> } />
             <Route path='/register' element={ <Register /> } />
         </Routes>
     )
