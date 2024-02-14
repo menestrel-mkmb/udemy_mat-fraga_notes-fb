9c5c7ad501d96925eccecc63a5c4c7b08e4d63c3
Michael Kevin <michaelkmb@hotmail.com>
2024-02-14 11:32:13 -0300

    misc Home classes for style;

--- notes-app/src/pages/Home/index.js
+++ notes-app/src/pages/Home/index.js
@@ -1,42 +1,46 @@
 import { useState } from "react";
-import "./index.module.css";
+import styles from "./index.module.css";
 
 export default function Home(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    return(
-    <main className="home-container">
-        <h1>Lista de tarefas</h1>
-        <span>Gerencia sua agenda de forma fácil.</span>
 
-        <form>
-            <section>
-                <label>
+    return(
+    <main className={`${styles.main__sect} ${styles.main}`}>
+        <h1 className={styles.main__title}>Lista de tarefas</h1>
+        <span className={styles.main__desc}>Gerencia sua agenda de forma fácil.</span>
+
+        <form className={styles.login__form}>
+            <section className={`${styles.inpt__sect} ${styles.email__sect}`}>
+                <label className={`${styles.inp__lbl} ${styles.email__lbl}`}>
                     e-mail:
                 </label>
                 <input
+                    className={`${styles.inp} ${styles.email__inp}`}
                     type="email"
                     placeholder="Digite seu e-mail"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
                 />
             </section>
-            <section>
-                <label>
+            <section className={`${styles.inpt__sect} ${styles.password__sect}`}>
+                <label className={`${styles.inp__lbl} ${styles.password__lbl}`}>
                     senha:
                 </label>
                 <input
+                    className={`${styles.inp} ${styles.password__inp}`}
+                    autoComplete="false"
                     type="password"
                     placeholder="Digite sua senha"
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                 />
-                <button
-                    type="submit"
-                >
-                    Entrar
-                </button>
             </section>
+            <button
+                type="submit"
+            >
+                Entrar
+            </button>
         </form>
     </main>
     )
