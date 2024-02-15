0c9658342c307dff5c38c4295f7a5ef03941e167
Michael Kevin <michaelkmb@hotmail.com>
2024-02-15 12:38:32 -0300

    feat Notes Component with fake noteList.map();

--- notes-app/src/pages/Notes/index.js
+++ notes-app/src/pages/Notes/index.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Notes(){
-    const [notes, setNotes] = useState([]);
+    const [notes, setNotes] = useState('');
     const [newNote, setNewNote] = useState('');
-    let notesList = [];
+    let notesList = ['essa é uma nota de exemplo', 'essa é outra nota'];
 
     const clearNewNote = () => {
         setNewNote('');
@@ -14,15 +14,24 @@ export default function Notes(){
         alert(newNote);
     }
 
+    const deleteNote = (index) => {
+        const resultNotesList = notesList.splice(index,1);
+    }
+
     useEffect(() => {
     }, [notes]);
 
-    return(<main>
-        <h1>Notes</h1>
+    return(
+    <main
+        className={`${styles.main} ${styles.main__sect}`}
+    >
+        <h1 className={`${styles.main__title}`}>Lista de Tarefas</h1>
         <article
             className={`${styles.notes__sect} ${styles.notes}`}
         >
-            <section>
+            <section
+                className={`${styles.newNote__sect} ${styles.newNote}`}
+            >
                 <h2>Nova anotação</h2>
                 <input
                     className={`${styles.newnote__inp} ${styles.inp}`}
@@ -30,18 +39,20 @@ export default function Notes(){
                     value={newNote}
                     onChange={e=>setNewNote(e.target.value)}
                 />
-                <button
-                    onClick={clearNewNote}
-                    className={`${styles.btn} ${styles.newNote__btn} ${styles.clear__btn}`}
-                >
-                    Limpar
-                </button>
-                <button
-                    onClick={addNewNote}
-                    className={`${styles.btn} ${styles.newNote__btn} ${styles.addNote__btn}`}
-                >
-                    Adicionar anotação
-                </button>
+                <section className={`${styles.btns__sect}`}>
+                    <button
+                        onClick={clearNewNote}
+                        className={`${styles.btn} ${styles.newNote__btn} ${styles.clear__btn}`}
+                    >
+                        Limpar
+                    </button>
+                    <button
+                        onClick={addNewNote}
+                        className={`${styles.btn} ${styles.newNote__btn} ${styles.addNote__btn}`}
+                    >
+                        Adicionar anotação
+                    </button>
+                </section>
             </section>
             <section>
                 <h2>
@@ -49,6 +60,23 @@ export default function Notes(){
                 </h2>
                 {notesList.length > 0 &&
                 (<article>
+                    {notesList.map( (note,index) => (
+                        <section key={index}
+                            className={`${styles.note} ${styles.note}${index}`}
+                        >
+                            <p
+                                className={`${styles.note__txt}`}
+                            >
+                                {note}
+                            </p>
+                            <button
+                                className={`${styles.noteDelete__btn}`}
+                                onClick={e=>deleteNote(index)}
+                            >
+                                X
+                            </button>
+                        </section>
+                    ))}
                 </article>)}
                 {!notesList.length > 0 &&
                 (<article>
